63f6de440f60cfa81b7b0ba54ba3286512c63c07
Abraham Thomas <dysruptmedia@gmail.com>
2014-03-19 16:32:46 +0000

    implemented recognising links in responses

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,16 @@
 module QuestionsHelper
+  def render_response(response)
+    res = response.response.split(' ')
+    i = 0
+    narr = []
+    while i < res.length
+      if res[i].start_with?('http')
+        narr << "<a href='#{res[i]}'>#{res[i]}</a>"
+      else 
+        narr << res[i]
+      end
+      i += 1
+    end 
+    return narr.join(' ').html_safe
+  end
 end
