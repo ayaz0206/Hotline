b0d0971548dec763fab49e7dd146047c316a98a3
biwek <biweks@gmail.com>
2014-03-27 11:17:07 +0000

    added hangout links to be visible

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -14,6 +14,21 @@ module QuestionsHelper
     return link_extract.join(' ').html_safe
   end
 
+  def render_question_hangout_link
+    des = @question.hangout_link.split(' ')
+    i = 0
+    link_extract = []
+    while i < des.length
+      if des[i].start_with?('http')
+        link_extract << "<a target='_blank' href='#{des[i]}'>#{des[i]}</a>"
+      else 
+        link_extract << des[i]
+      end
+      i += 1
+    end 
+    return link_extract.join(' ').html_safe
+  end
+
   def render_response(response)
     res = response.response.split(' ')
     i = 0
